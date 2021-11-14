ce64ffe85118cd242fc80943e3d686630bf52abe
Ali Ahmad <mian41935@gmail.com>
2021-11-14 15:16:09 +0500

    updtae HomeScreen.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,13 +3,13 @@ import {
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL
 } from '../constants/productConstants'
-import axios from 'axios'
+ import axios from 'axios'
 
-export const listProduct= () => async (dispatch) =>{
+export const listProducts = () => async (dispatch) =>{
  try {
-    dispatch({ type : PRODUCT_LIST_REQUEST })
+    dispatch({ type : PRODUCT_LIST_REQUEST });
     
-    const {data} = await axios.get('/api/products')
+    const {data} = await axios.get('/api/products');
     
     dispatch({ 
         type: PRODUCT_LIST_SUCCESS,
