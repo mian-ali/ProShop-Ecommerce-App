a8320908200123f3484e753bbe14cce2503e818a
Ali Ahmad <mian41935@gmail.com>
2021-11-16 14:37:23 +0500

    HomeScreen bring and get data from store

--- frontend/src/reducers/productReducres.js
+++ frontend/src/reducers/productReducres.js
@@ -3,14 +3,14 @@ import {PRODUCT_LIST_REQUEST,
        PRODUCT_LIST_FAIL
      }
     from '../constants/productConstants'
-export const  productListReducer=(state ={product :[] }, action )=>{
+export const  productListReducer=(state ={products :[] }, action )=>{
 
     switch(action.type){     
         case PRODUCT_LIST_REQUEST: 
-        return { loading : true ,  product:[] }
+        return { loading : true ,  products:[] }
 
         case PRODUCT_LIST_SUCCESS:
-        return { loading : false , product :action.payload}
+        return { loading : false , products :action.payload}
 
         case PRODUCT_LIST_FAIL:
         return {loading : false,  error: action.payload}
