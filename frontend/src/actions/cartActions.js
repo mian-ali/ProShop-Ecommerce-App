62589a1eeff3291b8babace54a80e3fd8e23dd8e
Ali Ahmad <mian41935@gmail.com>
2021-11-18 00:22:42 +0500

    cart Reducers

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+export const addToCart =(id , qty ) => async (dispatch, getState)=>{
+    const  {data} = await axios.get(`/api/products/${id}`) 
+
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload:{
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty
+        },
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
