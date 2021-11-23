014cc8e2e1a3c261f78bf2085bcdaea0b21506de
Ali Ahmad <mian41935@gmail.com>
2021-11-23 23:54:12 +0500

    user backend auth

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,9 @@
 import express from 'express'
 const router = express.Router();
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
+import {getProducts, getProductById} from '../controllers/productControllers.js'
 
-//@dec       Fehct all Products
-//@route     GET /api/products
-//@access    Public
-router.get('/', asyncHandler(async(req, res) => {
-    const products=await Product.find({})
-    // res.status(401)
-    // throw new Error("Not authorized") error get for products
-    // throw new Error("Some thing Wrong")
-    res.json(products);
-}))
-//@dec       Fehct single Product
-//@route     GET /api/products/:id
-//@access    Public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product =await Product.findById(req.params.id)
 
-    if (product) {
-        res.json(product)
-    }else{
-        res.status(404)
-        throw new Error("Product Not Found")
-    }
-    // res.json(product);
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductById)
 
 export default router
