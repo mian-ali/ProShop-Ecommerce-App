c6253da844c01ce3d83516eab88a06d9baf6c134
Ali Ahmad <mian41935@gmail.com>
2021-11-23 14:53:24 +0500

    cart Screen Complete

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM , CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart =(id , qty ) => async (dispatch, getState)=>{
     const  {data} = await axios.get(`/api/products/${id}`) 
@@ -18,4 +18,15 @@ export const addToCart =(id , qty ) => async (dispatch, getState)=>{
     })
 
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart =(id) => (dispatch , getState) => {
+
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload:id
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
 }
