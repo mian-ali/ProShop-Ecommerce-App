a72be0379ccf5e466f077e57a32c9ffac0477e0d
Ali Ahmad <mian41935@gmail.com>
2021-12-02 00:15:08 +0500

    Shipping Screen Complete

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
-import {CART_ADD_ITEM , CART_REMOVE_ITEM} from '../constants/cartConstants'
+import {CART_ADD_ITEM , CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/cartConstants'
 
-export const cartReducer=(state = {cartItems :[]}, action)=>{
+export const cartReducer=(state = {cartItems :[], shippingAddress :{}}, action)=>{
     switch (action.type){
         case CART_ADD_ITEM:
             const item = action.payload;
@@ -25,6 +25,12 @@ export const cartReducer=(state = {cartItems :[]}, action)=>{
                     ...state,
                     cartItems:state.cartItems.filter(x => x.product !== action.payload)
                 }
+
+                case CART_SAVE_SHIPPING_ADDRESS:
+                    return{ 
+                        ...state,
+                        shippingAddress: action.payload,
+                    }
 
             default :
              return state
