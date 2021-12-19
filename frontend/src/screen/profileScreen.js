86f687797882df6ea9f198745ffe24df3d81c2cc
Ali Ahmad <mian41935@gmail.com>
2021-12-20 00:21:32 +0500

    Showing order in user profile

--- frontend/src/screen/profileScreen.js
+++ frontend/src/screen/profileScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { Form, Button, Row, Col } from 'react-bootstrap'
+import {Form, Button, Row, Col, Table } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
 import { getUserDetails , updateUserProfile } from '../actions/userActions'
+import {listMyOrders} from '../actions/orderActions'
 
 const ProfileScreen = ({ location, history }) => {
   const [name, setName] = useState('')
@@ -23,6 +25,10 @@ const ProfileScreen = ({ location, history }) => {
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
   const { success } = userUpdateProfile
 
+  const orderListMy = useSelector((state) => state.orderListMy)
+  const { loading:loadingOrders, error:errorOrders, orders } = orderListMy
+
+
 
   useEffect(() => {
     if (!userInfo) {
@@ -30,7 +36,7 @@ const ProfileScreen = ({ location, history }) => {
     }else{
         if(!user.name){
             dispatch(getUserDetails('profile'))
-
+            dispatch(listMyOrders())
         }else{
             setName(user.name)
             setEmail(user.email)
@@ -102,8 +108,56 @@ const ProfileScreen = ({ location, history }) => {
       </Form>
   </Col>
   <Col md={9}>
-      <h2>My Order List</h2>
-  </Col>
+        <h2>My Orders</h2>
+        {loadingOrders ? (
+          <Loader />
+        ) : errorOrders ? (
+          <Message variant='danger'>{errorOrders}</Message>
+        ) : (
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <i className='fas fa-times' style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className='btn-sm' variant='light'>
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </Col>
 
   </Row>
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import {composeWithDevTools} from "redux-devtools-extension"
 import {productListReducer , productDetailsReducer} from './reducers/productReducres'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer , userRegisterReducer , userDetailsReducer , userUpdateProfileReducer } from './reducers/userReducers'
-import { orderCreateReducer , orderDetailsReducer , orderPayReducer } from './reducers/orderReducers'
+import { orderCreateReducer , orderDetailsReducer , orderPayReducer , orderListMyReducer } from './reducers/orderReducers'
 
 const reducer=combineReducers({
     productList: productListReducer,
@@ -17,6 +17,7 @@ const reducer=combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
 
 })
 // cartItems
