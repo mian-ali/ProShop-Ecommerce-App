d1b9eb1d5993002942f310a1ee53c3f19e86805b
Ali Ahmad <mian41935@gmail.com>
2021-12-28 11:50:04 +0500

    update amdin edit users

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -146,4 +146,50 @@ const deleteUser = asyncHandle(async(req,res)=>{
 })
 
 
-export  {authUsers , registerUser ,getUserProfile, updateUserProfile , getUser , deleteUser}
+//@dec       GET Users by id
+//@route     GET /api/users/:id
+//@access    Private/admin 
+
+const getUserById = asyncHandle(async(req,res)=>{
+ 
+    const user = await User.findById(req.params.id).select('-password')
+    if(user) {
+
+        res.json(user)
+
+    }else{
+        res.status(404)
+        throw new Error("User Not Found")
+    }
+})
+
+
+//@dec       Update user 
+//@route     Put /api/users/:id
+//@access    Private/admin
+
+const updateUser = asyncHandle(async(req,res)=>{
+ 
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        user.name =req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin=req.body.isAdmin || user.isAdmin
+        const updatedUser = await user.save()
+        res.json({ 
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+                    
+        })
+    }else{
+        res.status(404)
+        throw new Error("User Not Found")
+    }
+})
+
+
+
+export  {authUsers , registerUser ,getUserProfile, updateUserProfile , getUser , deleteUser ,getUserById ,updateUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 const router = express.Router();
-import { authUsers , registerUser ,getUserProfile , updateUserProfile , getUser , deleteUser} from '../controllers/userControllers.js'
+import { authUsers , registerUser ,getUserProfile , updateUserProfile , getUser , deleteUser , getUserById , updateUser} from '../controllers/userControllers.js'
 import {protect , admin } from '../middlewear/authMiddlewear.js'
 
 
 router.route('/').post( registerUser).get(protect, admin ,getUser)
 router.post('/login' , authUsers)
 router.route('/profile').get(protect,getUserProfile).put(protect ,updateUserProfile)
-router.route('/:id').delete(protect, admin ,deleteUser)
+router.route('/:id').delete(protect, admin ,deleteUser).get(protect, admin ,getUserById).put(protect ,admin,  updateUser) 
 
 
 export default router
