1d5cfc66e356bef8b36f1e3dc148483c8767b647
Ali Ahmad <mian41935@gmail.com>
2022-01-03 13:09:59 +0500

    Update Backend Deliverd Order

--- backend/controllers/OrderControllers.js
+++ backend/controllers/OrderControllers.js
@@ -80,6 +80,24 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     throw new Error('Order not found')
   }
 })
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
@@ -99,4 +117,4 @@ const getOrders = asyncHandler(async (req, res) => {
 })
 
 
-export {addOrderItems , getOrderById , updateOrderToPaid ,getMyOrders , getOrders}
+export {addOrderItems , getOrderById , updateOrderToPaid ,getMyOrders , getOrders , updateOrderToDelivered}
