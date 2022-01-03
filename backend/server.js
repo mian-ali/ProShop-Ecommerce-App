234823c9051b1b32b9610feafd50b9d53b501eee
Ali Ahmad <mian41935@gmail.com>
2022-01-04 00:34:59 +0500

    Upadte backend Product Review

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path';
 import express from 'express';     
 import dotenv from 'dotenv';
 import colors from 'colors' //import colors from npm
+import morgan from 'morgan';
 import connectDB from './config/db.js'; //db file import
 import productsRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
@@ -14,6 +15,12 @@ import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config();
 connectDB()
 const app = express()
+
+if(process.env.NODE_ENV==='development'){
+
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
 // app.use((req , res , next)=>{
