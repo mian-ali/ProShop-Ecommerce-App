891c89cc678ea195d0596db325ebfd045f23ac68
Muhammad Waqar <muhammad.waqar@mominsolutions.com>
2022-01-11 03:07:00 +0500

    Add ProductCarosel Slider Add

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+// import {Helmet} from 'react-helmet/'
 import { Row, Col } from "react-bootstrap";
 // import Products from "../products"
 import Product from "../Components/Product";
 import Message from '../Components/Message'
 import Loader from "../Components/Loader";
 import Paginate from "../Components/Paginate";
+// import Meta from "../Components/Meta";
+
 import ProductCarousel from "../Components/ProductCarousel";
 import { listProducts } from "../actions/productActions";
 
@@ -34,6 +37,8 @@ const pageNumber=match.params.pageNumber || 1
         <Message variant="danger">{error}</Message>
       ) : (
         <>
+    {/* <Meta /> */}
+
         <Row>
           {products.map((product) => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
